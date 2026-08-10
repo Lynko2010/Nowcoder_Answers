e28ddff3b8be5d37b7b46275f1b28b236926bbdd
Lynko <lynko2010@outlook.com>
2026-08-11 04:41:54 +0800

    NEW 解法3
    Refactor main function to read year and month until EOF and update YearMonth function to accept pointers.

--- BC54.c
+++ BC54.c
@@ -35,10 +35,9 @@ int YearMonth(int y, int m) {
 int main() {
     int year = 0;
     int month = 0;
-    //assert(year != NULL);
-    //assert(month != NULL);
-    scanf("%d %d", &year, &month);
-    printf("%d\n", YearMonth(year, month));
+    while(scanf("%d %d", &year, &month) != EOF) {
+        printf("%d\n", YearMonth(year, month));
+    }
     return 0;
 }
 
@@ -71,8 +70,54 @@ int YearMonth(int y, int m) {
 int main() {
     int year = 0;
     int month = 0;
-    scanf("%d %d", &year, &month);
-    int (*y)(int, int) = YearMonth;
-    printf("%d\n", y(year, month));
+    while(scanf("%d %d", &year, &month) != EOF) {
+        int (*y)(int, int) = YearMonth;
+        printf("%d\n", y(year, month));
+    }
+    return 0;
+}
+
+
+
+//解法3
+#include <stdio.h>
+#include <assert.h>
+int YearMonth(int* y, int* m) {
+    switch (*m) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            return 31;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            return 30;
+            break;
+        case 2:
+            if (*y % 400 == 0 || (*y % 4 == 0 && *y % 100 != 0)) {
+                return 29;
+            } else {
+                return 28;
+            default:
+                return -1;
+            }
+    }
+}
+
+int main() {
+    int y = 0;
+    int m = 0;
+    int* year = &y;
+    int* month = &m;
+    while(scanf("%d %d", year, month) != EOF) {
+        int (*ym)(int*, int*) = YearMonth;
+        printf("%d\n", ym(year, month));   
+    }
     return 0;
 }
